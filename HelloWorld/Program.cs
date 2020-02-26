3ecefa33058ce190dc1b461d8ce8687eba793e49
Ivan Phelps <ivanniothemannio@gmail.com>
2020-02-25 19:52:30 -0600

    added greeting to get name and fav color, asks if you want a random animal in your fav color

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -46,8 +46,24 @@ namespace HelloWorld
                 Console.WriteLine("Nǐ hǎo!!");
                 Console.ReadKey();
             }
+            Console.WriteLine("Please enter your name");
+
+            var name = Console.ReadLine();
+            Console.WriteLine($"Welcome to this feature rich application {name}! What is your favorite color?" );
+            var color = Console.ReadLine();
             
             var animals = new string[] { "Triceratops", "Gorilla", "Corgi", "Toucan", "Cat", "Dog" };
+
+            Random random = new System.Random();
+
+            var randomNum = random.Next(animals.Length);
+
+            Console.WriteLine($"So {name}, How would you like a {color} {animals[randomNum]}?");
+
+            var response = Console.ReadLine();
+
+            Console.WriteLine($"Seriously.... I offer you a {color} {animals[randomNum]} and all you have to say is {response}??? RUDE! Why don't you just go ahead and press enter to close this window and never open this application again you sick fuck!");
+            Console.ReadLine();
 
             foreach (var animal in animals)
             {
